a054d1c03c4a476097942cab56768221b39c405e
MJbae <bmj5208@gmail.com>
2020-12-07 12:01:39 +0900

    ADD: main Logic(Right 밀어내기)

--- src/step1/PushOutWords.java
+++ src/step1/PushOutWords.java
@@ -9,29 +9,51 @@
 package step1;
 
 public class PushOutWords {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		String inputString = "apple";
 		int inputCount = 3;
-		char inputDir = 'L';
-		
+		String inputDir = "r";
+		int lengthOfinputString = inputString.length();
+
 		char[] inputCharArray = inputString.toCharArray();
-		
-		for(int index = 0; index < inputCount; index++) {
-			char tempChar = inputCharArray[0];
-			System.out.println("tempChar " + tempChar);
-			
-			for(int j = 0; j < inputCharArray.length - 1; j++) {
-				inputCharArray[j] = inputCharArray[j + 1];
-				System.out.println("inputCharArray " + inputCharArray[j]);
+		inputDir = inputDir.toUpperCase();
+
+		if (inputDir.equals("R")) {
+			for (int i = lengthOfinputString; i > lengthOfinputString-inputCount; i--) {
+				char tempChar = inputCharArray[lengthOfinputString - 1];
+				System.out.println("tempChar " + tempChar);
+
+				for (int j = lengthOfinputString - 1; j > 0; j--) {
+					inputCharArray[j] = inputCharArray[j - 1];
+					System.out.println("inputCharArray " + inputCharArray[j]);
+				}
+
+				inputCharArray[0] = tempChar;
 			}
-			
-			inputCharArray[inputCharArray.length - 1] = tempChar;
+
+			String answer = String.valueOf(inputCharArray);
+			System.out.println("test R " + answer);
+
+		} else if (inputDir.equals("L")) {
+			for (int index = 0; index < inputCount; index++) {
+				char tempChar = inputCharArray[0];
+				System.out.println("tempChar " + tempChar);
+
+				for (int j = 0; j < lengthOfinputString - 1; j++) {
+					inputCharArray[j] = inputCharArray[j + 1];
+					System.out.println("inputCharArray " + inputCharArray[j]);
+				}
+
+				inputCharArray[inputCharArray.length - 1] = tempChar;
+			}
+
+			String answer = String.valueOf(inputCharArray);
+			System.out.println("test L " + answer);
+		} else {
+			System.out.println("유효한 명령어를 입력하십시오");
 		}
-		
-		String answer = String.valueOf(inputCharArray);
-		System.out.println("test " + answer);
+
 	}
 }
