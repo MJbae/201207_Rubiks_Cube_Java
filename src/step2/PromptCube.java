63932ab0e7e162eb9db6d1a0007a08b6e6cd1a08
MJbae <bmj5208@gmail.com>
2020-12-08 20:39:12 +0900

    Refactor: 주요로직(사용자 입력값 중 작은따옴표(') 포함한 명령 구분)에 대한 메소드화

--- src/step2/PromptCube.java
+++ src/step2/PromptCube.java
@@ -15,11 +15,30 @@ public class PromptCube {
 		System.out.println("");
 	}
 
-	public static void main(String[] args) {
-		// 초기값 선언
-		char[][] twoDimensionalCube = { { 'R', 'R', 'W' }, { 'R', 'R', 'W' }, { 'R', 'R', 'W' } };
-		final int SIZE_OF_CUBE = twoDimensionalCube[0].length;
+	public String[] splitInputString(String input) {
+
+		StringBuffer inputSplit = new StringBuffer(input);
+		int indexOfSplit = 1;
 		
+		// String input의 각 문자에 대해 순회
+		for (int i = 0; i < input.length() - 1; i++) {
+			// 작은따옴표가 있는 경우, 작은 따옴표 기준 우측에 공백 문자 삽입
+			if (input.charAt(i + 1) == 39) {
+				indexOfSplit += 1;
+				inputSplit.insert(indexOfSplit, " ");
+				i++;
+				indexOfSplit += 2;
+				// 일반 문자(작은따옴표가 없는)일 경우, 다음 문자에 작은 따옴표가 없다면 문자 기준 우측에 공백 문자 삽입
+			} else {
+				inputSplit.insert(indexOfSplit, " ");
+				indexOfSplit += 2;
+			}
+		}
+		
+		return new String(inputSplit).split(" ");
+	}
+
+	public void executePrompt(char[][] twoDimensionalCube, int SIZE_OF_CUBE) {
 		Scanner scanner = new Scanner(System.in);
 		PromptCube prompt = new PromptCube();
 		PushTwoDimensionalCube pushCube = new PushTwoDimensionalCube();
@@ -31,31 +50,14 @@ public class PromptCube {
 			// 사용자 입력
 			System.out.print("  CUBE> ");
 			String input = scanner.nextLine();
-			StringBuffer inputSplit = new StringBuffer(input);
 			// 실행 중단 로직
-			if(input.equals("Q")) break;
-			
-			// String input의 각 문자에 대해 순회
-			// 작은따옴표가 있는 경우, 작은 따옴표 기준 우측에 공백 문자 삽입
-			// 일반 문자(작은따옴표가 없는)일 경우, 다음 문자에 작은 따옴표가 없다면 문자 기준 우측에 공백 문자 삽입
-			// 공백문자 기준으로 String[]에 명령어 할당
-			int indexOfSplit = 1;
-			for(int i = 0; i < input.length() - 1; i++) {
-				if(input.charAt(i + 1) == 39) {
-					indexOfSplit += 1;
-					inputSplit.insert(indexOfSplit, " ");
-					i++;
-					indexOfSplit += 2;
-				} else {
-					inputSplit.insert(indexOfSplit, " ");
-					indexOfSplit += 2;
-				}
-			}
-			
-			String[] splitStringArray = new String(inputSplit).split(" ");
+			if (input.equals("Q"))
+				break;
+
+			// inputSplit에 대해 공백문자 기준으로 분할하여 모든 명령어를 String array에 할당
+			String[] splitStringArray = splitInputString(input);
 
 			System.out.println("");
-
 			// 각각의 명령 수행
 			for (String inputDir : splitStringArray) {
 				// 명령 실행 메소드
@@ -68,7 +70,14 @@ public class PromptCube {
 		}
 		scanner.close();
 		System.out.println("  Bye~");
-
 	}
 
+	public static void main(String[] args) {
+		// 초기값 선언
+		char[][] twoDimensionalCube = { { 'R', 'R', 'W' }, { 'R', 'R', 'W' }, { 'R', 'R', 'W' } };
+		final int SIZE_OF_CUBE = twoDimensionalCube[0].length;
+
+		PromptCube prompt = new PromptCube();
+		prompt.executePrompt(twoDimensionalCube, SIZE_OF_CUBE);
+	}
 }
