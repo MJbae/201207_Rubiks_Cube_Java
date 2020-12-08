c5bb0a600369ee9f16b87e7170af829bbf8bcc7e
MJbae <bmj5208@gmail.com>
2020-12-08 14:26:07 +0900

    Add: U, U', B, B' 기능 추가

--- src/step2/PushTwoDimensionalCube.java
+++ src/step2/PushTwoDimensionalCube.java
@@ -5,12 +5,34 @@
 
 package step2;
 
+import step1.*;
+
 public class PushTwoDimensionalCube {
-	
-	private static char[][] TWO_DIMENSIONAL_CUBE = {{'R', 'R', 'W'}, {'R', 'R', 'W'}, {'R', 'R', 'W'}};
+
+	final static int SIZE_OF_CUBE = 3;
 
 	public static void main(String[] args) {
-		for(char[] oneDimensionalCube : TWO_DIMENSIONAL_CUBE) {
+		char[][] twoDimensionalCube = { { 'R', 'R', 'W' }, { 'R', 'R', 'W' }, { 'R', 'R', 'W' } };
+		String inputDir = "B'";
+
+		PushOutWords pushWords = new PushOutWords();
+
+		switch(inputDir) {
+		case "U":
+			pushWords.getLeftpushedString(twoDimensionalCube[0], 1, SIZE_OF_CUBE);
+			break;
+		case "U'":
+			pushWords.getRightpushedString(twoDimensionalCube[0], 1, SIZE_OF_CUBE);
+			break;
+		case "B":
+			pushWords.getLeftpushedString(twoDimensionalCube[SIZE_OF_CUBE - 1], 1, SIZE_OF_CUBE);
+			break;
+		case "B'":
+			pushWords.getRightpushedString(twoDimensionalCube[SIZE_OF_CUBE - 1], 1, SIZE_OF_CUBE);
+			break;
+		}
+		
+		for(char[] oneDimensionalCube : twoDimensionalCube) {
 			for(char elementOfCube : oneDimensionalCube) {
 				System.out.print(elementOfCube + " ");
 			}
