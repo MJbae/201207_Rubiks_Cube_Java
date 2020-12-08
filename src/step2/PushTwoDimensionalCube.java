3ecd78f075b504f23d7979d7293c89d37cc3159d
MJbae <bmj5208@gmail.com>
2020-12-08 12:37:10 +0900

    Add: 2차원 배열 활용하여 초기값 할당

--- src/step2/PushTwoDimensionalCube.java
+++ src/step2/PushTwoDimensionalCube.java
@@ -1,9 +1,21 @@
+//핵심 아이디어
+//1. 2차원 배열을 활용하여 초기값을 할당
+//2. 가장 왼쪽 및 오른쪽 회전 구현
+//3. 가장 위쪽 및 아래쪽 회전 구현
+
 package step2;
 
 public class PushTwoDimensionalCube {
+	
+	private static char[][] TWO_DIMENSIONAL_CUBE = {{'R', 'R', 'W'}, {'R', 'R', 'W'}, {'R', 'R', 'W'}};
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(char[] oneDimensionalCube : TWO_DIMENSIONAL_CUBE) {
+			for(char elementOfCube : oneDimensionalCube) {
+				System.out.print(elementOfCube + " ");
+			}
+			System.out.println("");
+		}
 
 	}
 
