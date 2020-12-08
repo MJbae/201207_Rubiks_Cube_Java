cbd48bf8c138263366ef5e4f0360e9eeb5de615c
MJbae <bmj5208@gmail.com>
2020-12-08 20:18:05 +0900

    Add: Right rotate 명령(U' or B')과 Left rotate 명령(U or B) 구분 로직 추가

--- src/step2/PromptCube.java
+++ src/step2/PromptCube.java
@@ -19,8 +19,8 @@ public class PromptCube {
 		// 초기값 선언
 		char[][] twoDimensionalCube = { { 'R', 'R', 'W' }, { 'R', 'R', 'W' }, { 'R', 'R', 'W' } };
 		final int SIZE_OF_CUBE = twoDimensionalCube[0].length;
+		
 		Scanner scanner = new Scanner(System.in);
-
 		PromptCube prompt = new PromptCube();
 		PushTwoDimensionalCube pushCube = new PushTwoDimensionalCube();
 
@@ -28,18 +28,36 @@ public class PromptCube {
 		prompt.printResult(twoDimensionalCube);
 
 		while (true) {
-
 			// 사용자 입력
 			System.out.print("  CUBE> ");
 			String input = scanner.nextLine();
+			StringBuffer inputSplit = new StringBuffer(input);
+			// 실행 중단 로직
 			if(input.equals("Q")) break;
 			
-			String[] inputSplit = input.split("");
+			// String input의 각 문자에 대해 순회
+			// 작은따옴표가 있는 경우, 작은 따옴표 기준 우측에 공백 문자 삽입
+			// 일반 문자(작은따옴표가 없는)일 경우, 다음 문자에 작은 따옴표가 없다면 문자 기준 우측에 공백 문자 삽입
+			// 공백문자 기준으로 String[]에 명령어 할당
+			int indexOfSplit = 1;
+			for(int i = 0; i < input.length() - 1; i++) {
+				if(input.charAt(i + 1) == 39) {
+					indexOfSplit += 1;
+					inputSplit.insert(indexOfSplit, " ");
+					i++;
+					indexOfSplit += 2;
+				} else {
+					inputSplit.insert(indexOfSplit, " ");
+					indexOfSplit += 2;
+				}
+			}
+			
+			String[] splitStringArray = new String(inputSplit).split(" ");
 
 			System.out.println("");
 
 			// 각각의 명령 수행
-			for (String inputDir : inputSplit) {
+			for (String inputDir : splitStringArray) {
 				// 명령 실행 메소드
 				pushCube.getPushedCube(twoDimensionalCube, inputDir, SIZE_OF_CUBE);
 
