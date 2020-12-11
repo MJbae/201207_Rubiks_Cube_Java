03ddcd05675ed02e66025d9bee11c73707cbb5d0
MJbae <bmj5208@gmail.com>
2020-12-11 18:39:49 +0900

    Refactor: getPushedCube 내 R or R' 처리 로직을 하위 메소드로 분할

--- src/step2/PushTwoDimensionalCube.java
+++ src/step2/PushTwoDimensionalCube.java
@@ -6,7 +6,6 @@ public class PushTwoDimensionalCube {
 	// TODO: 자바에서 메소드를 매개변수로 전달하는 방법 학습 후 R,R',L,L' 중복코드 제거하기
 	public char[][] getPushedCube(char[][] twoDimensionalCube, String inputDir, int SIZE_OF_CUBE) {
 
-		PushOutWords pushWords = new PushOutWords();
 		char[] tempCube = new char[SIZE_OF_CUBE];
 
 		// else if로 중간 구분하고 switch 사용 X
@@ -16,28 +15,31 @@ public class PushTwoDimensionalCube {
 			 twoDimensionalCube[SIZE_OF_CUBE - 1] = getUpBottomPushed(twoDimensionalCube[SIZE_OF_CUBE - 1], SIZE_OF_CUBE, inputDir);
 		 } else if(inputDir.contains("L")) {
 			 twoDimensionalCube = getLeftPushed(twoDimensionalCube, SIZE_OF_CUBE, inputDir, tempCube);
-		 } 
+		 } else if(inputDir.contains("R")) {
+			 twoDimensionalCube = getRightPushed(twoDimensionalCube, SIZE_OF_CUBE, inputDir, tempCube);
+		 }
 		 
-		switch (inputDir) {
-		case "R":
-			for (int i = 0; i < SIZE_OF_CUBE; i++)
-				tempCube[i] = twoDimensionalCube[i][SIZE_OF_CUBE - 1];
-			tempCube = pushWords.getLeftpushedString(tempCube, 1, SIZE_OF_CUBE);
-			for (int j = 0; j < SIZE_OF_CUBE; j++)
-				twoDimensionalCube[j][SIZE_OF_CUBE - 1] = tempCube[j];
-			break;
-		case "R'":
-			for (int i = 0; i < SIZE_OF_CUBE; i++)
-				tempCube[i] = twoDimensionalCube[i][SIZE_OF_CUBE - 1];
-			tempCube = pushWords.getRightpushedString(tempCube, 1, SIZE_OF_CUBE);
-			for (int j = 0; j < SIZE_OF_CUBE; j++)
-				twoDimensionalCube[j][SIZE_OF_CUBE - 1] = tempCube[j];
-			break;
-		default:
-			break;
-		}
-
 		return twoDimensionalCube;
+	}
+	
+	public char[][] getRightPushed(char[][] inputArray, int SIZE_OF_CUBE, String inputDir, char[] tempCube) {
+		PushOutWords pushWords = new PushOutWords();
+
+		for (int i = 0; i < SIZE_OF_CUBE; i++)
+			tempCube[i] = inputArray[i][SIZE_OF_CUBE - 1];
+		
+		if (inputDir.equals("R")) {
+			tempCube = pushWords.getLeftpushedString(tempCube, 1, SIZE_OF_CUBE);
+		} else if (inputDir.equals("R'")) {
+			tempCube = pushWords.getRightpushedString(tempCube, 1, SIZE_OF_CUBE);
+		} else {
+			inputArray = null;
+		}
+		
+		for (int j = 0; j < SIZE_OF_CUBE; j++)
+			inputArray[j][SIZE_OF_CUBE - 1] = tempCube[j];
+		
+		return inputArray;
 	}
 	
 	public char[][] getLeftPushed(char[][] inputArray, int SIZE_OF_CUBE, String inputDir, char[] tempCube) {
