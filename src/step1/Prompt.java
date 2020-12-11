7ea49ee61a8af5be79c693f26199364b7017a77e
MJbae <bmj5208@gmail.com>
2020-12-11 14:39:41 +0900

    Docs: 사용자 입력값 처리에 대한 주석 추가

--- src/step1/Prompt.java
+++ src/step1/Prompt.java
@@ -17,15 +17,18 @@ public class Prompt {
 			System.out.print("  > ");
 
 			String input = scanner.nextLine();
+			
 			// -1 입력 시 순회문 탈출
-			if( input.equals("-1")) break;
+			if(input.equals("-1")) break;
 			
 			// 입력값에 대해 공백문자를 기준으로 나눔
 			String[] inputSplit = input.split(" ");
 			
+			// 첫번째 입력값(단어)에 대해 char Array 형태로 변환
 			char[] inputCharArray = inputSplit[0].toCharArray();
+			// 두번째 입력값(정수)에 대해 int 형태로 변환
 			inputCount = Integer.parseInt(inputSplit[1]);
-			// 회전 방향 입력값을 대문자로 변환
+			// 세번째 입력값(이동방향)에 대해 대문자로 변환
 			inputDir = inputSplit[2].toUpperCase();;
 
 			PushOutWords pushWords = new PushOutWords();
