d3e83b8dd391e685e710e28550bdc9dc8a9d4128
MJbae <bmj5208@gmail.com>
2020-12-11 14:34:35 +0900

    Refactor: executePrompt 메소드 내 불필요 코드 제거에 따른 코드 간결성 향상

--- src/step1/Prompt.java
+++ src/step1/Prompt.java
@@ -3,9 +3,9 @@ package step1;
 import java.util.Scanner;
 
 public class Prompt {
-
+	
+	
 	public void executePrompt() {
-		String inputString = "";
 		int inputCount = 0;
 		String inputDir = "";
 		String answer = "";
@@ -22,25 +22,21 @@ public class Prompt {
 			
 			// 입력값에 대해 공백문자를 기준으로 나눔
 			String[] inputSplit = input.split(" ");
-			inputString = inputSplit[0];
+			
+			char[] inputCharArray = inputSplit[0].toCharArray();
 			inputCount = Integer.parseInt(inputSplit[1]);
-			inputDir = inputSplit[2];
-
-			int lengthOfinputString = inputString.length();
-			PushOutWords pushWords = new PushOutWords();
-
-			char[] inputCharArray = inputString.toCharArray();
 			// 회전 방향 입력값을 대문자로 변환
-			inputDir = inputDir.toUpperCase();
+			inputDir = inputSplit[2].toUpperCase();;
 
+			PushOutWords pushWords = new PushOutWords();
 			// 오른쪽으로 문자를 회전시키는 경우
 			if ((inputCount >= 0 && inputDir.equals("R")) || (inputCount < 0 && inputDir.equals("L"))) {
 				inputCount = inputCount < 0 ? inputCount * (-1) : inputCount;
-				answer = pushWords.getRightpushedString(inputCharArray, inputCount, lengthOfinputString);
+				answer = pushWords.getRightpushedString(inputCharArray, inputCount, inputCharArray.length);
 			// 왼쪽으로 문자를 회전시키는 경우
 			} else if ((inputCount >= 0 && inputDir.equals("L")) || (inputCount < 0 && inputDir.equals("R"))) {
 				inputCount = inputCount < 0 ? inputCount * (-1) : inputCount;
-				answer = pushWords.getLeftpushedString(inputCharArray, inputCount, lengthOfinputString);
+				answer = pushWords.getLeftpushedString(inputCharArray, inputCount, inputCharArray.length);
 			} else {
 				System.out.println("유효한 명령어를 입력하십시오");
 			}
