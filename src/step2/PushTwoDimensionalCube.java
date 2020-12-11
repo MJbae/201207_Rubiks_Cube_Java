d816da7a9b80a32dae9238c4736f7ba208218bc6
MJbae <bmj5208@gmail.com>
2020-12-11 18:20:19 +0900

    Refactor: getPushedCube 내 UU'BB' 처리 로직을 하위 메소드로 분할

--- src/step2/PushTwoDimensionalCube.java
+++ src/step2/PushTwoDimensionalCube.java
@@ -9,13 +9,28 @@ public class PushTwoDimensionalCube {
 		PushOutWords pushWords = new PushOutWords();
 		char[] tempCube = new char[SIZE_OF_CUBE];
 
+		// else if로 중간 구분하고 switch 사용 X
+		 if(inputDir.contains("U")){
+			 twoDimensionalCube[0] = getUpBottomPushed(twoDimensionalCube[0], SIZE_OF_CUBE, inputDir);
+		 } else if(inputDir.contains("B")) {
+			 twoDimensionalCube[SIZE_OF_CUBE - 1] = getUpBottomPushed(twoDimensionalCube[SIZE_OF_CUBE - 1], SIZE_OF_CUBE, inputDir);
+		 }
+		 
 		switch (inputDir) {
-		case "U":
-			twoDimensionalCube[0] = pushWords.getLeftpushedString(twoDimensionalCube[0], 1, SIZE_OF_CUBE);
-			break;
-		case "U'":
-			twoDimensionalCube[0] = pushWords.getRightpushedString(twoDimensionalCube[0], 1, SIZE_OF_CUBE);
-			break;
+//		case "U":
+//			twoDimensionalCube[0] = pushWords.getLeftpushedString(twoDimensionalCube[0], 1, SIZE_OF_CUBE);
+//			break;
+//		case "U'":
+//			twoDimensionalCube[0] = pushWords.getRightpushedString(twoDimensionalCube[0], 1, SIZE_OF_CUBE);
+//			break;
+//		case "B":
+//			twoDimensionalCube[SIZE_OF_CUBE - 1] = pushWords.getLeftpushedString(twoDimensionalCube[SIZE_OF_CUBE - 1],
+//					1, SIZE_OF_CUBE);
+//			break;
+//		case "B'":
+//			twoDimensionalCube[SIZE_OF_CUBE - 1] = pushWords.getRightpushedString(twoDimensionalCube[SIZE_OF_CUBE - 1],
+//					1, SIZE_OF_CUBE);
+//			break;
 		case "R":
 			for (int i = 0; i < SIZE_OF_CUBE; i++)
 				tempCube[i] = twoDimensionalCube[i][SIZE_OF_CUBE - 1];
@@ -44,18 +59,22 @@ public class PushTwoDimensionalCube {
 			for (int j = 0; j < SIZE_OF_CUBE; j++)
 				twoDimensionalCube[j][0] = tempCube[j];
 			break;
-		case "B":
-			twoDimensionalCube[SIZE_OF_CUBE - 1] = pushWords.getLeftpushedString(twoDimensionalCube[SIZE_OF_CUBE - 1],
-					1, SIZE_OF_CUBE);
-			break;
-		case "B'":
-			twoDimensionalCube[SIZE_OF_CUBE - 1] = pushWords.getRightpushedString(twoDimensionalCube[SIZE_OF_CUBE - 1],
-					1, SIZE_OF_CUBE);
-			break;
 		default:
 			break;
 		}
 
 		return twoDimensionalCube;
+	}
+
+	public char[] getUpBottomPushed(char[] inputArray, int SIZE_OF_CUBE, String inputDir) {
+		PushOutWords pushWords = new PushOutWords();
+
+		if (inputDir.equals("U") || inputDir.equals("B")) {
+			return pushWords.getLeftpushedString(inputArray, 1, SIZE_OF_CUBE);
+		} else if (inputDir.equals("U'") || inputDir.equals("B'")) {
+			return pushWords.getRightpushedString(inputArray, 1, SIZE_OF_CUBE);
+		} else {
+			return null;
+		}
 	}
 }
