412e6b11f15b6f45912b0391af20d45b0e403927
MJbae <bmj5208@gmail.com>
2020-12-13 06:15:23 +0900

    Docs: 무작위 섞기 기능에 대한 주석 구체화

--- src/step3/MixCube.java
+++ src/step3/MixCube.java
@@ -2,23 +2,27 @@ package step3;
 
 public class MixCube {
 	
-	public static final String[] INPUTS = PromptRubiks.VALID_INPUTS;
+	private static final String[] INPUTS = PromptRubiks.VALID_INPUTS;
 	
-	public int getRandomNum() {
+	//메소드: 무작위 숫자 생성하여 정수형태로 반환 
+	public int getRandomInt() {
 		double randomNum = Math.random();
 		return (int)(randomNum * 10);
 	}
 	
+	// 메소드: 유효명령어 배열의 요소를 무작위로 선택하여 해당 명령어로 큐브를 회전 
 	public char[][][] getMixedCube(char[][][] rubiksCube) {
 		PushRubiksCube pushRubiks = new PushRubiksCube();
-		int RandomNum = getRandomNum();
-		String[] mixedInputs = new String[RandomNum];
+		int randomInt = getRandomInt();
+		String[] mixedInputs = new String[randomInt];
 		
-		for(int i = 0; i < RandomNum; i++) {
-			int RandomNum2 = getRandomNum();
-			mixedInputs[i] = INPUTS[RandomNum2];
+		// 유효한 명령어 배열(INPUTS)에서 요소를 무작위로 선택하여 mixedInputs 배열에 할당
+		for(int i = 0; i < randomInt; i++) {
+			int randomInt2 = getRandomInt();
+			mixedInputs[i] = INPUTS[randomInt2];
 		}
 		
+		// 무작위로 선택한 실행명령어 배열의 모든 요소에 따라 큐브 회전
 		for (String inputDir : mixedInputs) 
 			rubiksCube = pushRubiks.getPushedRubiks(rubiksCube, inputDir);
 
@@ -28,5 +32,4 @@ public class MixCube {
 
 		return rubiksCube;
 	}
-
 }
