b7f7b10c85e5ff7b739427030942d975c01a449b
MJbae <bmj5208@gmail.com>
2020-12-29 11:46:54 +0900

    Chore: Spring Project 추가

---
+++ rubiks_cube_refactoring/src/test/java/GentleM/rubiks_cube_refactoring/RubiksCubeRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package GentleM.rubiks_cube_refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubiksCubeRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
