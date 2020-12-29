c5596f83d695d5b26ecd9108d338a61e54d0c445
MJbae <bmj5208@gmail.com>
2020-12-29 16:53:59 +0900

    Test: isValidInput Test에 ParameterizedTest 적용

--- rubiks_cube_refactoring/src/test/java/GentleM/rubiks_cube_refactoring/PromptRubiksTest.java
+++ rubiks_cube_refactoring/src/test/java/GentleM/rubiks_cube_refactoring/PromptRubiksTest.java
@@ -1,14 +1,20 @@
 package GentleM.rubiks_cube_refactoring;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PromptRubiksTest {
-    @Test
-    void isValidInput() {
+
+    @ParameterizedTest(name = "[{index}] {argumentsWithNames}")
+    @CsvSource({"true, R", "true, R'", "true, R2", "true, L2", "true, L", "true, F'", "true, B", "true, U", "true, U2", "false, 2R", "fasle, K"})
+    void isValidInput(ArgumentsAccessor argumentsAccessor) {
         PromptRubiks prompt = new PromptRubiks();
-        String inputString = "L2";
-        assertEquals(true, prompt.isValidInput(inputString), inputString + " is not Valid");
+        String inputString = argumentsAccessor.getString(1);
+        assertEquals(argumentsAccessor.getBoolean(0), prompt.isValidInput(inputString), inputString + " is not Valid");
     }
 }
